2502bca8ce4c41dd028439e8d3e7a7f4c36cd65b
Joshua Earnest <joshuaearnest@JPs-MacBook-Pro.fios-router.home>
2020-10-21 09:23:53 -0400

    1.2

---
+++ 1/1.2.js
@@ -0,0 +1,31 @@
+// determine is a string is a permutation of another
+// early exit if the strings are not the same length
+// create an array of 0's with length 128 (assuming 128 ASCII character set)
+// iterate through first string, incrementing the array item corresponding to each character's ASCII integer each time the character is found
+// iterate through second string, decrementing the array item corresponding to each character's ASCII integer each time the character is found
+// early exist if an array item becomes negative during iteration through the second string
+
+const permuation = (strA, strB) => {
+  if (strA.length !== strB.length) return false;
+
+  const letters = new Array(128).fill(0);
+  for (let i = 0; i < strA.length; i++) {
+    const charCodeA = strA.charAt(i).charCodeAt(0);
+    letters[charCodeA]++;
+  }
+
+  for (let i = 0; i < strB.length; i++) {
+    const charCodeB = strB.charAt(i).charCodeAt(0);
+    letters[charCodeB]--;
+    if (letters[charCodeB] < 0) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+console.log(permuation("stringA", "Agnirts"));
+console.log(permuation("stringA", "StringA"));
+console.log(permuation("stringA", "stringAA"));
+console.log(permuation("stringA", "stringB"));
